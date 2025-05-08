72b5fe59449cc54c835e0a452653af768c51f0db
MOHAMEDAZURUDDIN <ajstackdev7@gmail.com>
2025-05-08 11:04:15 +0530

    updated files for development

--- frontend/src/styles/icon/index.js
+++
@@ -1,9 +0,0 @@
-import { List, styled } from "@mui/material";
-
-
-export const MyList = styled(List)(({theme})=>({
-display:'flex',
-flexGrow:3,
-justifyContent:'center',
-alignItems:'center',
-}))
